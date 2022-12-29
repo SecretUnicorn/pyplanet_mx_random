1c76d3a83c8fe70db92dc3c2d723f9eef89ac1b1
Nicklas <nicky@nicklas-ahlers.de>
2022-12-29 03:50:07 +0100

    add migration

---
+++ migrations/00_addname.py
@@ -0,0 +1,11 @@
+name_field = CharField(default='unknown name')
+
+from ..models import UserPoints
+
+def upgrade(migrator: SchemaMigrator):
+    migrate(
+        migrator.add_column(UserPoints._meta.db_table, 'name', name_field)
+    )
+
+def downgrade(migrator: SchemaMigrator):
+    pass
